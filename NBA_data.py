6a1aba375e57e4515ef3afdc162600f9f5e3b2cd
JerryMcG <76828978+JerryMcG@users.noreply.github.com>
2025-02-13 19:00:51 -0500

    defining team and player lists

--- NBA_data.py
+++ NBA_data.py
@@ -4,11 +4,46 @@ import os
 #assign variable for data load and the path
 data = os.path.join('Resources', '2018-19_pbp.csv')
 
+#initialize total row count
+total_rows = 0
+#initialize team list
+teams = []
+#create player list
+players = []
+
 #open NBA data
 with open(data) as NBA_2018_2019:
     #read the file
     NBA_read = csv.reader(NBA_2018_2019)
     #print the header row 
     headers = next(NBA_read)
-    print(headers)
-#analysis of the data
+
+    #create a total count of data rows
+    for row in NBA_read:
+        #add to total row count
+        total_rows += 1
+
+        #TEAM DATA
+        #where to find the abbreviation field
+        team_abb = row[14]
+
+        #if team name does not match existing team..
+        if team_abb not in teams:
+            #add team to list
+            teams.append(team_abb)
+        
+        #PLAYER DATA
+        #where to find player name
+        player_name = row[13]
+
+        #if player name does not match existing player..
+        if player_name not in players:
+            #add player to list
+            players.append(player_name)
+
+#print header row
+print(headers) 
+#print unique team list
+print(teams)
+#print unique player list
+print(players)
